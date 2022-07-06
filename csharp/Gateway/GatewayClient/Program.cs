2618e6641c62aa21db2211cfd807898f77b5354c
Are-Oelsner <are_oelsner@brown.edu>
2022-07-06 11:49:01 -0400

    feat(csharp): add interactive getter and setters to csharp client and server
    fully implemented getters and setters in the csharp client and server
    added interactivity to the client, taking in user input for requests
    Added c# style documentation to client and server

--- csharp/Gateway/GatewayClient/Program.cs
+++ csharp/Gateway/GatewayClient/Program.cs
@@ -10,16 +10,77 @@ using Gateway;
 namespace GatewayClient
 {
     class Program
-    {
+    { 
+        /// <summary>
+        /// Client guide function that takes provided input and makes a request from the server
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request">specifies which electrode's state is being requested</param>
+        private static void guide_get_electrode_state(Gateway.Gateway.GatewayClient client, Gateway.ElectrodeNumber request)
+        {
+            Console.WriteLine("\tgetting electrode {0} state...", request.Number);
+            var electrodeState = client.getElectrodeState(request);
+            if (electrodeState.State == -1)
+            {
+                Console.WriteLine("\terror: invalid input: {0} is not a valid electrode", request.Number);
+            }
+            else
+            {
+                Console.WriteLine("\telectrode {0} state: {1}", request.Number, electrodeState.State);
+            }
+        }
+        /// <summary>
+        /// Client guide function that takes provided input and makes a request from the server
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request">specifies which electrode's state is being set and to what</param>
+        private static void guide_set_electrode_state(Gateway.Gateway.GatewayClient client, Gateway.Electrode request)
+        {
+            Console.WriteLine("\tsetting electrode {0} state to {1}...", request.Number, request.State);
+            var electrodeState = client.setElectrodeState(request);
+            if (electrodeState.State == -1)
+            {
+                Console.WriteLine("\terror: invalid input: {0} is not a valid electrode", request.Number);
+            }
+            else
+            {
+                Console.WriteLine("\telectrode {0} state set to: {1}", request.Number, electrodeState.State);
+            }
+        }
+        /// <summary>
+        /// Main function for Client, handles user input
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             Channel channel = new Channel("127.0.0.1:30051", ChannelCredentials.Insecure); // TODO change port to 50051 to match python implementation?
-            
             var client = new Gateway.Gateway.GatewayClient(channel);
 
-            var reply = client.getElectrodeState(new ElectrodeNumber { Number = 0 });
-            Console.WriteLine(reply.State);
-            
+            var user_input = "";
+            string[] command;
+            while(user_input != "quit")
+            {
+                Console.Write("please enter command: ");
+                user_input = Console.ReadLine();
+                command = user_input.Split(" ");
+                if (command[0] == "help")
+                {
+                    Console.WriteLine("please refer to README for use guide");
+                }
+                else if (command[0] == "get") {
+                    guide_get_electrode_state(client, new Gateway.ElectrodeNumber { Number = Int32.Parse(command[1]) });
+                }
+                else if (command[0] == "set")
+                {
+                    guide_set_electrode_state(client, new Gateway.Electrode { Number = Int32.Parse(command[1]), State = Int32.Parse(command[2]) });
+                }
+                else
+                {
+                    Console.WriteLine("\tinvalid input: {0} is not a valid command", user_input);
+                }
+
+            }
+
             channel.ShutdownAsync().Wait();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
--- csharp/Gateway/GatewayServer/Program.cs
+++ csharp/Gateway/GatewayServer/Program.cs
@@ -3,58 +3,81 @@
  * C# implementation of gRPC Gateway Server 
  */
 
+
 using System;
-using System.Threading.Tasks; // I think I would only need this for async calls?
+using System.Threading.Tasks;
 using Grpc.Core;
 using Gateway;
 
 namespace GatewayServer
 {
-
-    // <summary>Device simulating neural implant with four electrodes </summary>
-    // Stores states for four electrodes
+    /// <summary>
+    /// Device simulating neural implant with four electrodes
+    /// </summary>
     class Device
     {
-        public int[] electrodes;
-
-        // <summary> Constructor, initializes default electrode states to 0 </summary>
-        public Device()
+        /// <summary>
+        /// Static array storing electrode states as ints
+        /// </summary>
+        public static int[] electrodes = {0, 0, 0, 0};
+        /// <summary>
+        /// Returns the state of the requested electrode
+        /// </summary>
+        /// <param name="electrodeNumber">Specifies which electrode's state is requested</param>
+        /// <returns>(ElectrodeState) State of requested electrode</returns>
+        public static ElectrodeState get_electrode_state(ElectrodeNumber electrodeNumber)
         {
-            this.electrodes = new int[] { 0, 0, 0, 0 };
-        }
-
-        // <summary> Returns the state of the requested electrode </summary>
-        // <param name="electrodeNumber"> Specifies which electrode's state is requested</param>
-        public ElectrodeState get_electrode_state(ElectrodeNumber electrodeNumber)
-        {
+            Console.WriteLine("\tgetting electrode {0} state...", electrodeNumber.Number);
             if(electrodeNumber.Number < electrodes.Length)
             {
+                Console.WriteLine("\treturning electrode {0} state: {1}", electrodeNumber.Number, electrodes[electrodeNumber.Number]);
                 return new ElectrodeState { State = electrodes[electrodeNumber.Number] };
             }
+            Console.WriteLine("\terror: electrode {0} not found. returning -1", electrodeNumber.Number);
             return new ElectrodeState { State = -1 };
         }
-
-        public ElectrodeState set_electrode_state(Electrode electrode)
+        /// <summary>
+        /// Sets specified electrode's state to the provided state
+        /// </summary>
+        /// <param name="electrode">message that contains an int State and int Number</param>
+        /// <returns>(ElectrodeState) message containing int value of the requested electrode state, or -1 if invalid input</returns>
+        public static ElectrodeState set_electrode_state(Electrode electrode)
         {
-            if(electrode.Number < electrodes.Length)
-            {
+            Console.WriteLine("\tsetting electrode {0} state to {1}...", electrode.Number, electrode.State);
+            if (electrode.Number < electrodes.Length)
+            { 
                 electrodes[electrode.Number] = electrode.State;
+                Console.WriteLine("\treturning electrode {0} state: {1}", electrode.Number, electrodes[electrode.Number]);
                 return new ElectrodeState { State = electrodes[electrode.Number] };
             }
+            Console.WriteLine("\terror: electrode {0} not found. returning -1", electrode.Number);
             return new ElectrodeState { State = -1 };
         }
     }
+    /// <summary>
+    /// GatewayServer is used by the server to implement a gRPC service
+    /// </summary>
     class GatewayServer : Gateway.Gateway.GatewayBase
     {
-        // Server side handler of the getElectrodeState RPC
+        /// <summary>
+        /// Server side handler of the getElectrodeState RPC
+        /// </summary>
+        /// <param name="request">specifies which electrode's state is being requested</param>
+        /// <param name="context"></param>
+        /// <returns>(ElectrodeState) state of requested electrode</returns>
         public override Task<ElectrodeState> getElectrodeState(ElectrodeNumber request, ServerCallContext context)
         {
-            return Task.FromResult(new ElectrodeState { State = -1}); // TODO change state value passed in?
+            return Task.FromResult(Device.get_electrode_state(request));
         }
-        // TODO Server side handler for the setElectrodeState RPC
+        /// <summary>
+        /// Server side handler for the setElectrodeState RPC
+        /// </summary>
+        /// <param name="request">specifies which electrode's state is being set and the value to set it to</param>
+        /// <param name="context"></param>
+        /// <returns>(ElectrodeState) state of electrode after it is set</returns>
         public override Task<ElectrodeState> setElectrodeState(Electrode request, ServerCallContext context)
         {
-            return Task.FromResult(new ElectrodeState { State = -1 }); // TODO change value
+            return Task.FromResult(Device.set_electrode_state(request)); 
         }
 
     }
@@ -62,7 +85,10 @@ namespace GatewayServer
     public class Program
     {
         const int Port = 30051; // TODO should this be 50051?
-
+        /// <summary>
+        /// Main method for Server, sets up and starts server
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             Server server = new Server
